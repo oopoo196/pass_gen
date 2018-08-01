d5ef6d1122031700d478c46088c720a9946fe5a4
EeOneDown <rentgen.stalker@yandex.ru>
2018-08-01 18:06:17 +0300

    max count changed

--- gen_pass.py
+++ gen_pass.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     args = parse_args()
 
     pass_len = args.length or 18
-    count = min(args.count or 1, 10)
+    count = min(args.count or 1, 15)
     is_spec_symbols = args.special or False
 
     for i in range(count):
